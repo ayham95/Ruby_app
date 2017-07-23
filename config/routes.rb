7e1920d8b7284c8ac7fb96f4e27f16cb8d65cda2
Ayham Orfali <orfali.ayham@gmail.com>
2017-07-23 19:32:14 +0300

    test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   get 'test/index'
+  root 'user#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
